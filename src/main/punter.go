58ff9bcb65edbf9a295a8cb49ab5e2cd742cbb45
Your Name <you@example.com>
2017-08-05 01:14:35 +0300

    Full online support with stubs for game strategy.

--- src/main/punter.go
+++ src/main/punter.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"encoding/json"
 	"flag"
+	"game"
 	"io"
 	"log"
 	"net"
@@ -25,18 +26,112 @@ type You struct {
 	You string `json:"you"`
 }
 
+type Site struct {
+	Id int `json:"id"`
+}
+
+// The Map structure from server is inconvinient, this is a wrapper.
+type Map struct {
+	Sites  []Site       `json:"sites"`
+	Rivers []game.River `json:"rivers"`
+	Mines  []int        `json:"mines"`
+}
+
+func toGameMap(m *Map) game.Map {
+	sites := make([]int, len(m.Sites), len(m.Sites))
+	for i, site := range m.Sites {
+		sites[i] = site.Id
+	}
+	return game.Map{Sites: sites, Rivers: m.Rivers, Mines: m.Mines}
+}
+
+type Setup struct {
+	Punter  int `json:"punter"`
+	Punters int `json:"punters"`
+	Map     Map `json:"map"`
+}
+
+type Ready struct {
+	Ready int `json:"ready"`
+}
+
+type ClaimMove struct {
+	Punter int `json:"punter"`
+	Source int `json:"source"`
+	Target int `json:"target"`
+}
+
+type PassMove struct {
+	Punter int `json:"punter"`
+}
+
+type Move struct {
+	Claim *ClaimMove `json:"claim,omitempty"`
+	Pass  *PassMove  `json:"pass,omitempty"`
+}
+
+type Moves struct {
+	Moves []Move `json:"moves"`
+}
+
+func toGameMove(m *Move) game.Move {
+	if m.Pass != nil {
+		return game.MakePassMove(m.Pass.Punter)
+	}
+	claim := m.Claim
+	return game.MakeClaimMove(claim.Punter, claim.Source, claim.Target)
+}
+
+func fromGameMove(m *game.Move) (r Move) {
+	switch m.Type {
+	case game.Claim:
+		r.Claim = &ClaimMove{Punter: m.Punter, Source: m.Source, Target: m.Target}
+	case game.Pass:
+		r.Pass = &PassMove{Punter: m.Punter}
+	default:
+		log.Fatal("Unknown move type:", m.Type)
+	}
+	return
+}
+
+func toGameMoves(moves []Move) (r []game.Move) {
+	n := len(moves)
+	r = make([]game.Move, n, n)
+	for i, m := range moves {
+		r[i] = toGameMove(&m)
+	}
+	return
+}
+
+type Score struct {
+	Punter int `json:"punter"`
+	Score  int `json:"score"`
+}
+
+type Stop struct {
+	Moves  []Move  `json:"move"`
+	Scores []Score `json:"scores"`
+}
+
+type Step struct {
+	Moves   *Moves `json:"move"`
+	Stop    *Stop  `json:"stop"`
+	Timeout *int   `json:"timeout"`
+}
+
 func sendMessage(w *bufio.Writer, message interface{}) {
 	bs, err := json.Marshal(message)
 	if err != nil {
-		panic(err)
+		log.Fatal("Can't send message:", err)
 	}
 	ss := string(bs)
 
 	io.WriteString(w, strconv.Itoa(len(ss))+":"+ss)
 	w.Flush()
+	return
 }
 
-func recvMessage(r *bufio.Reader, message interface{}) (err error) {
+func recvMessage(r *bufio.Reader, message interface{}) {
 	length, err := r.ReadString(':')
 	if err != nil {
 		return
@@ -53,7 +148,10 @@ func recvMessage(r *bufio.Reader, message interface{}) (err error) {
 		return
 	}
 
-	return json.Unmarshal(bytes, message)
+	err = json.Unmarshal(bytes, message)
+	if err != nil {
+		log.Fatal("Can't receive message:", err)
+	}
 }
 
 func handshake(r *bufio.Reader, w *bufio.Writer) {
@@ -61,21 +159,57 @@ func handshake(r *bufio.Reader, w *bufio.Writer) {
 	sendMessage(w, me)
 
 	var you You
-	err := recvMessage(r, &you)
-	if err != nil {
-		log.Fatal("Can't do hanshake with server:", err)
-	}
+	recvMessage(r, &you)
 
 	if me.Me != you.You {
-		log.Fatal("Expected:", me.Me, "received:", you.You)
+		log.Fatal("Expected:", me.Me, " received:", you.You)
 	}
 
 	log.Println("Successful handshake")
 }
 
+func setup(r *bufio.Reader, w *bufio.Writer, p *game.Player) {
+	var setup Setup
+	recvMessage(r, &setup)
+
+	gm := toGameMap(&setup.Map)
+
+	log.Println("Punter id:", setup.Punter)
+	log.Println("Number of punters:", setup.Punters)
+	log.Println("Game map:", gm)
+
+	p.Setup(setup.Punter, setup.Punters, gm)
+
+	sendMessage(w, Ready{Ready: setup.Punter})
+}
+
+func interact(r *bufio.Reader, w *bufio.Writer, p *game.Player) {
+	for {
+		var step Step
+		recvMessage(r, &step)
+		if step.Moves != nil {
+			move := p.MakeMove(toGameMoves(step.Moves.Moves))
+			log.Println("Making move:", move)
+			sendMessage(w, fromGameMove(&move))
+			continue
+		}
+		if step.Stop != nil {
+			log.Println("Final scores:", step.Stop.Scores)
+			break
+		}
+		if step.Timeout != nil {
+			log.Println("Timeout:", *step.Timeout)
+			continue
+		}
+		log.Fatal("Unknown state")
+	}
+}
+
 func main() {
 	log.SetFlags(0)
 	flag.Parse()
+
+	var player game.Player
 
 	if *flagPort < 0 {
 		log.Println("Running in offline mode")
@@ -92,5 +226,7 @@ func main() {
 		writer := bufio.NewWriter(conn)
 
 		handshake(reader, writer)
+		setup(reader, writer, &player)
+		interact(reader, writer, &player)
 	}
 }
